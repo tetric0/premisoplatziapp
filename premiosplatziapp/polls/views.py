725f56b4546249160a443b953aa8ebe8cc057ac7
victor.munoz <victor.munoz@upc.edu>
2022-03-04 12:55:11 +0100

    Cambios hasta el video 19 del Curso Basico de Django (Platzi).

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -2,4 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world")
+    return HttpResponse("Estás en la página principal de Premios Platzi App")
+
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
