be2abf57fe4138de88c07242393deb775e64757f
victor.munoz <victor.munoz@upc.edu>
2022-03-08 10:14:58 +0100

    Video 22 del Curso basico de Django (Platzi) acabado: Segundo template (Detalle de pregunta con metodo get_object_or_404) acabado.

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
 from .models import Question
@@ -11,7 +11,10 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {
+        "question": question
+    })
 
 
 def results(request, question_id):
