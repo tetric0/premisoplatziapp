48b7396a4126f860d93cf912e56d3feefba07fb3
victor.munoz <victor.munoz@upc.edu>
2022-03-09 11:28:54 +0100

    Video 4 del 'Curso de Django Intermedio. Testing, static Files, Django admin' acabado: Primer test + Reto.

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -1,3 +1,26 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_questions(self):
+        """was_published_recently returns False for questions whose pub_date is in the future"""
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(question_text="¿Quien es el mejor Course Director de Platzi?", pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+    def test_was_published_recently_with_recent_questions_published_passed_24_hours(self):
+        """was_published_recently returns False for questions whose pub_date more than the last 24 hours"""
+        time = timezone.now() - datetime.timedelta(hours=25)
+        past_question = Question(question_text="Question text", pub_date=time)
+        self.assertIs(past_question.was_published_recently(), False)
+    
+    def test_was_published_recently_with_questions_published_now(self):
+        """was_published_recently returns True for questions whose pub_date is now"""
+        time = timezone.now()
+        past_question = Question(question_text="Question text", pub_date=time)
+        self.assertIs(past_question.was_published_recently(), True)
