8ceb4f2ab750b94cffee05719ad7be511a0bcdd0
victor.munoz <victor.munoz@upc.edu>
2022-03-10 20:35:25 +0100

    Video 8 del 'Curso de Django Intermedio. Testing, static Files, Django admin' acabado: Reto1 completado (Añadidos los test a la vista Results de Question ('test_no_questions', 'test_future_question' y 'test_past_question') con la modificacion pertiernente en la vista.

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -130,3 +130,34 @@ class QuestionDetailViewTests(TestCase):
         url = reverse('polls:detail', args=(past_question.id,))
         response = self.client.get(url)
         self.assertContains(response, past_question.question_text)
+
+class QuestionResultsViewTests(TestCase):
+
+    def test_no_question(self):
+        """
+        The Results View of a Question that doesn't exist
+        must return a 404 Error (Not Found)
+        """
+        url = reverse("polls:results", args=(1,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_future_question(self):
+        """
+        The Result View of a Question with a 'pub_date' in the future
+        must return a 404 Error (Not found)
+        """
+        future_question = create_question(question_text="Future Question", days=5)
+        url = reverse("polls:results", args=(future_question.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_past_question(self):
+        """
+        The Result View of a Question with a 'pub_date' in the past
+        must display the Question's text
+        """
+        past_question = create_question(question_text="Past question", days=-30)
+        url = reverse("polls:results", args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response, past_question.question_text)
