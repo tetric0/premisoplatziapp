1bd53861751be6ed053f2fe568708e7c5f7a26ed
victor.munoz <victor.munoz@upc.edu>
2022-03-11 09:10:39 +0100

    Videos 12 y 13 del 'Curso de Django Intermedio. Testing, static Files, Django admin' acabado: Añadidas configuraciones en el Admin de Django mostrar (list_display), filtrar (list_filter) y añadir un campo de busqueda por atributos en el Admin de Question + Añadida la estructura HTML base de la Vista Index para mejorar su apariencia.

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import Choice, Question
 
+
 class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
@@ -9,5 +10,8 @@ class ChoiceInline(admin.StackedInline):
 class QuestionAdmin(admin.ModelAdmin):
     fields = ['pub_date', 'question_text']
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 admin.site.register(Question, QuestionAdmin)
